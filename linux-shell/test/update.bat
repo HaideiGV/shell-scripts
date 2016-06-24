d908a34eac3cedb0a0267f767b7f6ec03d5c0e1d
HaideiGV <haideigv@gmail.com>
2016-06-24 17:33:47 +0300

    win

--- linux-shell/test/update.bat
+++ linux-shell/test/update.bat
@@ -1,12 +1,33 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-Rem psql -d testdb -U haidei -c "select * from querylist" > outw.txt
+psql -d testdb -U postgres -c "select * from querylist" > dbversion.txt
 
-Setlocal EnableDelayedExpansion
-set LIST=
-for %%i in (*createtable.sql) do set LIST=!LIST! %%i
-echo %LIST%
+set version=
+for /F "skip=2" %%G in (dbversion.txt) do (
+	set version=!version!%%G
+	set sign=%version:~-19,-18%
+	@echo %sign%
+	if %sign%=="." (set version=%version:~0,-20%)
+	else (set version=%version:~0,-19%)
+	) %%G
 
-for %%num in ("%LIST%") do (
-	echo %%num
-)
+echo %version%| findstr /r "[0-9]*"
+
+
+for %%i in (*.createtable.sql) do (
+	echo %%i > filelist.txt
+	)
+
+
+for /F "skip=2" %%s in (filelist.txt) do (
+	echo %%s| findstr /r "[0-9]*"
+	rem echo %file_version%
+	if %version% LSS %file_version% (
+		psql -d testdb -U postgres -a -f "%%s"
+		set max_file=%file_version%.createtable.sql
+		)
+	else (echo "All scripts are updated!")
+	)
+
+rem psql -d testdb -U postgres -c "update querylist set queryname='"%max_file%"'"
