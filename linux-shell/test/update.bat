e1d3b85a20ae690d5937be764d824773dc95688e
HaideiGV <HaideiGV@gmail.com>
2016-06-26 22:51:00 +0300

    working windows bat file

--- linux-shell/test/update.bat
+++ linux-shell/test/update.bat
@@ -1,33 +1,27 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
+color 0e
 
-psql -d testdb -U postgres -c "select * from querylist" > dbversion.txt
+psql -d testdb -U postgres -c "select * from querylist limit 1;" > dbversion.txt
+
 
 set version=
-for /F "skip=2" %%G in (dbversion.txt) do (
-	set version=!version!%%G
-	set sign=%version:~-19,-18%
-	@echo %sign%
-	if %sign%=="." (set version=%version:~0,-20%)
-	else (set version=%version:~0,-19%)
-	) %%G
-
-echo %version%| findstr /r "[0-9]*"
-
-
-for %%i in (*.createtable.sql) do (
-	echo %%i > filelist.txt
+for /f %%g in ('findstr /r "[0-9]*\.\createtable.sql" dbversion.txt') do (
+	set version=%%g
 	)
 
+echo Current version is %version%
 
-for /F "skip=2" %%s in (filelist.txt) do (
-	echo %%s| findstr /r "[0-9]*"
-	rem echo %file_version%
-	if %version% LSS %file_version% (
-		psql -d testdb -U postgres -a -f "%%s"
-		set max_file=%file_version%.createtable.sql
-		)
-	else (echo "All scripts are updated!")
+for /r %%i in (*createtable.sql) do (echo %%~nxi >> files.txt)
+
+set last=
+for /f %%s in ('findstr "[0-9]*\.*" ./files.txt') do (
+	if "%version%" LSS "%%s" (
+		psql -d testdb -U postgres -a -f "%%s" > nul
+		set last=%%s
+	) else (
+		echo "All scripts are updated!"
 	)
+)
 
-rem psql -d testdb -U postgres -c "update querylist set queryname='"%max_file%"'"
+psql -d testdb -U postgres -c "update querylist set queryname='"%last%"'"
