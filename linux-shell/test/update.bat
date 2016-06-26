f30ada10f4d37e15e7a2f8e21f420ebded5709be
HaideiGV <HaideiGV@gmail.com>
2016-06-27 00:47:19 +0300

    win

--- linux-shell/test/update.bat
+++ linux-shell/test/update.bat
@@ -6,7 +6,7 @@ psql -d testdb -U postgres -c "select * from querylist limit 1;" > dbversion.txt
 
 
 set version=
-for /f %%g in ('findstr /r "[0-9]*\.\createtable.sql" dbversion.txt') do (
+for /f %%g in ('findstr /r "[0-9]*\createtable.sql" dbversion.txt') do (
 	set version=%%g
 	)
 
@@ -14,14 +14,17 @@ echo Current version is %version%
 
 for /r %%i in (*createtable.sql) do (echo %%~nxi >> files.txt)
 
-set last=
+set last=%version%
 for /f %%s in ('findstr "[0-9]*\.*" ./files.txt') do (
+	echo Local %%s|findstr /r /c:"[0-9]*\.*"
 	if "%version%" LSS "%%s" (
 		psql -d testdb -U postgres -a -f "%%s" > nul
 		set last=%%s
 	) else (
-		echo "All scripts are updated!"
+		rem echo Already updated!
 	)
 )
 
 psql -d testdb -U postgres -c "update querylist set queryname='"%last%"'"
+
+del files.txt
